dd526080bf0194ecdf9587aaae8b12c08ca8ebc3
Josiah Jezrael Guevarra <guevarrajosiahjezrael@gmail.com>
2025-12-02 11:24:45 +0800

    Added: Routing

---
+++ app/login/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+
+export default function pLogin() {
+  return (
+    
+    <section>
+
+      <div>
+        <Link href="/"> Back to Home </Link>
+      </div>
+
+      <div>Login Page</div>
+      
+    </section>
+
+
+
+  );
+}
